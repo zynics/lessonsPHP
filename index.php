2bd3a6ca73d8d2576fc6b5552a94d9136483a59d
zynics <nic@newmail.ru>
2017-09-13 07:42:17 +0300

    work

--- index.php
+++ index.php
@@ -5,18 +5,18 @@
 <?php
 $l=4;
 $arraynum =0;
-$array = array(433,5465,43534,3424,465,4645,2342)
-echo "<table>";
+$array = array(433,5465,43534,3424,465,4645,2342);
+echo "<table border='1'>";
 echo "<tr>";
 while($i <=$l){
-    echo"<td>"
-    echo "$array{$arraynum}";
-    $arraynum=$arraynum+1;
-    $i=$i+1;
-    echo</td>
+    echo"<td>";
+    echo "$array[$arraynum]";
+    $arraynum++;
+    $i++;
+    echo "</td>";
 }
 echo "</tr>";
 echo "</table>";
-
+?>
 </body>
 </html>
