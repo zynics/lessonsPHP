7efeec7eccc387471d476b4e641f59f93ae9b4fd
zynics <nic@newmail.ru>
2017-09-13 07:25:41 +0300

    php and html

--- index.php
+++ index.php
@@ -4,9 +4,19 @@
 
 <?php
 $l=4;
-array(433,5465,43534,3424)
-echo;
-i
-?>
+$arraynum =0;
+$array = array(433,5465,43534,3424,465,4645,2342)
+echo "<table>";
+echo "<tr>";
+while($i <=$l){
+    echo"<td>"
+    echo "$array{$arraynum}";
+    $arraynum=$arraynum+1;
+    $i=$i+1;
+    echo</td>
+}
+echo "</tr>";
+echo "</table>";
+
 </body>
 </html>
