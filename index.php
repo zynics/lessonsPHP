6938686e145dc3985864c23243d4a60cb60a30b9
zynics <nic@newmail.ru>
2017-09-14 16:59:33 +0300

    Lesson 2 go

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
-<html><header></header><body>
+
 <form name="countArray" action="" method="post">
     <input name="num" type="number" >
     <input name="num2" type="number" >
     <input type="submit" name="run" value="sub">
 </form>
 <?php
+echo "123";
 $l=$_POST["num"];
 $l2=$_POST["num2"];
 $array = array();
-echo "<table border='1'><tr>";
+echo "<table border='1'>";
 for($i=0; $i<$l; $i++){
     echo"<tr>";
-          for($i2=0; $i2<$l2; $i++){
+          for($i2=0; $i2<$l2; $i2++){
               echo"<td>";
               $array[$i][$i2]=rand(1,9999);
               echo"$array[$i][$i2]";
@@ -19,6 +20,4 @@ for($i=0; $i<$l; $i++){
           }
     echo"</tr>";
 }
-echo"</tr></table>";
-?>
-</body></html>
+echo "</table>";
