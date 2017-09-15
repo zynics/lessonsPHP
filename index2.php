9da3cacd12da678a71dbe1612c4f2048d613ee92
zynics <nic@newmail.ru>
2017-09-15 16:16:26 +0300

    Lesson 3.1

--- index2.php
+++ index2.php
@@ -6,19 +6,26 @@
 </form>
 <?php
 
-$l = $_POST["num"];
-$l2 = $_POST["num2"];
-//$l = 3;
-//$l2 = 3;
+//$l = $_POST["num"];
+//$l2 = $_POST["num2"];
+$l = 3;
+$l2 = 5;
+
 $array = [];
 echo ">";
 for($i = 0; $i<$l; $i++){
 
         for($i2 = 0; $i2 < $l2; $i2++) {
-            if ($i == $i2)
+            //if ($i == $i2)
+            //    $array[$i][$i2] = 1;
+            //else
+            //   $array[$i][$i2] = 0;
+
+            if ($i2== $l2)
                 $array[$i][$i2] = 1;
             else
                 $array[$i][$i2] = 0;
+
         }
 }
 echo "</table>";
